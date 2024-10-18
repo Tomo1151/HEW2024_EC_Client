34e1820e4fec99009841cefc0c9b169a8fe8f219
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-10-18 15:57:01 +0900

    post fetch

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,26 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Post = ({ content }) => {
-  const post = {
-    author: {
-      user_name: "test_0",
-    },
-    content: "テスト投稿",
-    created_at: "2022-01-01T00:00:00Z",
-  };
-
-  const profile_image = "https://placeholder.com/150";
-
+const Post = ({ username, nickname, icon_link, content, created_at }) => {
   return (
     <section className="bg-white my-8 p-8 shadow-lg rounded-md">
       <div className="flex">
         <Link
-          href={`/users/${post.author.user_name}`}
+          href={`/users/${username}`}
           className="h-fit hover:brightness-[.75] rounded-full duration-200"
         >
           <Image
-            src={profile_image}
+            src={icon_link || "https://placeholder.com/150"}
             width="50"
             height="50"
             className="rounded-full mr-4"
@@ -29,20 +19,20 @@ const Post = ({ content }) => {
         </Link>
         <div>
           <Link
-            href={`/users/${post.author.user_name}`}
+            href={`/users/${username}`}
             className="font-bold hover:underline tracking-[.075em]"
           >
-            {post.author?.user_name || "名無し"}
+            {nickname || username}
           </Link>
           <p className="select-none font-bold opacity-35">
-            {new Date(post.created_at).toLocaleString("ja-JP")}
+            {new Date(created_at).toLocaleString("ja-JP")}
           </p>
         </div>
       </div>
-      <p className="mt-8 px-2 pb-2">{post.content}</p>
-      <Link href="/tags/" className="text-blue-400 mt-8 px-2 hover:underline">
+      <p className="mt-8 px-2 pb-2">{content}</p>
+      {/* <Link href="/tags/" className="text-blue-400 mt-8 px-2 hover:underline">
         #{content}
-      </Link>
+      </Link> */}
     </section>
   );
 };
--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -1,17 +1,50 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useTabContext } from "@/context/TabContext";
+import { fetchBaseURL } from "@/config/fetchConfig";
 import Post from "@/components/Post";
 
 const Timeline = () => {
   const { activeTab, tabContents } = useTabContext();
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = async (latest) => {
+    try {
+      const response = await fetch(fetchBaseURL + "/posts");
+      const resJson = await response.json();
 
+      if (resJson.success) {
+        setPosts(resJson.data);
+        console.log(resJson);
+      }
+    } catch (err) {
+      console.log("e");
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await fetchPosts();
+    })();
+  }, []);
+  console.log(posts);
   return (
     <>
+      {posts.map((post, idx) => (
+        <Post
+          key={idx}
+          username={post.author.username}
+          nickname={post.author.nickname}
+          icon_link={post.author.icon_link}
+          content={post.content}
+          created_at={post.created_at}
+        />
+      ))}
+      {/* <Post content={tabContents[activeTab]} />
       <Post content={tabContents[activeTab]} />
       <Post content={tabContents[activeTab]} />
-      <Post content={tabContents[activeTab]} />
-      <Post content={tabContents[activeTab]} />
+      <Post content={tabContents[activeTab]} /> */}
     </>
   );
 };
