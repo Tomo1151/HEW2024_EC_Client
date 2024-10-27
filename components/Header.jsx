dd780a8c3ac0c4282d641b01fe61121c367288f9
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-10-27 10:05:58 +0900

    activate intercepting routes

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,6 +24,8 @@ import {
   CreateRounded,
 } from "@mui/icons-material";
 
+import Link from "next/link";
+
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import {
 //   faHouse,
@@ -39,35 +41,37 @@ const Header = () => {
   const margin = 5;
 
   return (
-    <Drawer
-      component="header"
-      variant="permanent"
-      ModalProps={{
-        keepMounted: true, // Better open performance on mobile.
-      }}
-      sx={{
-        width: {
-          xs: drawerWidth / 4,
-          sm: drawerWidth / 3,
-          md: drawerWidth / 2,
-          xl: drawerWidth,
-        },
-        flexShrink: 0,
-        whiteSpace: "nowrap",
-        "& .MuiDrawer-paper": {
+    <>
+      <Drawer
+        component="header"
+        variant="permanent"
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
           width: {
-            xs: drawerWidth / 3,
+            xs: drawerWidth / 4,
             sm: drawerWidth / 3,
-            md: drawerWidth / 1.5,
+            md: drawerWidth / 2,
             xl: drawerWidth,
           },
-          boxSizing: "border-box",
-        },
-        minWidth: "fit-content",
-      }}
-      // className="fixed top-0 h-full basis-1/3 bg-main-theme z-30"
-    >
-      {/* <Box sx={{ backgroundColor: "primary.main" }}>
+          flexShrink: 0,
+          whiteSpace: "nowrap",
+          "& .MuiDrawer-paper": {
+            width: {
+              xs: drawerWidth / 3,
+              sm: drawerWidth / 3,
+              md: drawerWidth / 1.5,
+              xl: drawerWidth,
+            },
+            boxSizing: "border-box",
+            zIndex: 39,
+          },
+          minWidth: "fit-content",
+        }}
+        // className="fixed top-0 h-full basis-1/3 bg-main-theme z-30"
+      >
+        {/* <Box sx={{ backgroundColor: "primary.main" }}>
         <Image
           src="/appri_logo.png"
           width={150}
@@ -78,216 +82,219 @@ const Header = () => {
         />
       </Box> */}
 
-      <List
-        sx={{
-          "& .MuiListItemText-primary": {
-            fontSize: "1.25em", // primaryテキストのサイズ
-          },
-          "& .MuiListItemIcon-root": {
-            justifyContent: "center", // アイコンの位置
-            mx: {
-              xs: "auto",
-              sm: "auto",
-              md: "auto",
-              xl: "2em",
-            }, // アイコンの左側のpadding
-            minWidth: "50px", // アイコンの幅
-          },
-        }}
-      >
-        <ListItem
+        <List
           sx={{
-            backgroundColor: "primary.main",
-            pt: "1em",
-            pl: {
-              xl: "3.5em",
+            "& .MuiListItemText-primary": {
+              fontSize: "1.25em", // primaryテキストのサイズ
             },
-            mb: "1em",
-            minHeight: "80px",
+            "& .MuiListItemIcon-root": {
+              justifyContent: "center", // アイコンの位置
+              mx: {
+                xs: "auto",
+                sm: "auto",
+                md: "auto",
+                xl: "2em",
+              }, // アイコンの左側のpadding
+              minWidth: "50px", // アイコンの幅
+            },
           }}
         >
-          {/* <ListItemButton> */}
-          <Image
-            src="/appri_logo.png"
-            width={150}
-            height={150}
-            alt="アプリロゴ"
-            priority={true}
-            // className={`mx-auto px-2 pt-3 drop-shadow-md cursor-pointer`}
-          />
-          {/* </ListItemButton> */}
-        </ListItem>
-        <ListItem>
-          <ListItemButton href="/">
-            <ListItemIcon>
-              <AccountCircleRounded
+          <ListItem
+            sx={{
+              backgroundColor: "primary.main",
+              pt: "1em",
+              pl: {
+                xl: "3.5em",
+              },
+              mb: "1em",
+              minHeight: "80px",
+            }}
+          >
+            {/* <ListItemButton> */}
+            <Image
+              src="/appri_logo.png"
+              width={150}
+              height={150}
+              alt="アプリロゴ"
+              priority={true}
+              // className={`mx-auto px-2 pt-3 drop-shadow-md cursor-pointer`}
+            />
+            {/* </ListItemButton> */}
+          </ListItem>
+          <ListItem>
+            <ListItemButton href="/">
+              <ListItemIcon>
+                <AccountCircleRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="プロフィール"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="プロフィール"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton href="/">
-            <ListItemIcon>
-              <HomeRounded
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton href="/">
+              <ListItemIcon>
+                <HomeRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="タイムライン"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="タイムライン"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <LiveTvRounded
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <LiveTvRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="ライブ配信"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="ライブ配信"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <NotificationsRounded
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <NotificationsRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="通知"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="通知"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <ShoppingCartRounded
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <ShoppingCartRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="カート"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="カート"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton>
-            <ListItemIcon>
-              <CheckBoxRounded
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <CheckBoxRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="購入履歴"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="購入履歴"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <Divider sx={{ mt: "3em" }} />
-        <ListItem>
-          <ListItemButton href="/register">
-            <ListItemIcon>
-              <CreateRounded
+            </ListItemButton>
+          </ListItem>
+          <Divider sx={{ mt: "3em" }} />
+          <ListItem>
+            <ListItemButton href="/register">
+              <ListItemIcon>
+                <CreateRounded
+                  sx={{
+                    fontSize: "2em",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary="新規登録"
                 sx={{
-                  fontSize: "2em",
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "block",
+                  },
                 }}
               />
-            </ListItemIcon>
-            <ListItemText
-              primary="新規登録"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton href="/login">
-            <ListItemIcon>
-              <LoginRounded
-                sx={{
-                  fontSize: "2em",
-                }}
-              />
-            </ListItemIcon>
-            <ListItemText
-              primary="ログイン"
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "block",
-                },
-              }}
-            />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Drawer>
+            </ListItemButton>
+          </ListItem>
+          <Link href="/login">
+            <ListItem>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LoginRounded
+                    sx={{
+                      fontSize: "2em",
+                    }}
+                  />
+                </ListItemIcon>
+                <ListItemText
+                  primary="ログイン"
+                  sx={{
+                    display: {
+                      xs: "none",
+                      sm: "none",
+                      md: "block",
+                    },
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+    </>
   );
 };
 
