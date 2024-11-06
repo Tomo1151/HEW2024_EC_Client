f397d7ef83534f5e0c278ff857ade5fd0b8e65f4
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-11-06 14:37:00 +0900

    add config: output - standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "placeholder.com",
-			},
-		],
-	},
-	reactStrictMode: false,
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "placeholder.com",
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
