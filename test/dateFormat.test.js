094757b487eb25023103e28ac5422eab4d30b996
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-11-20 15:58:57 +0900

    create test files

---
+++ test/dateFormat.test.js
@@ -0,0 +1,114 @@
+import { test, expect, describe } from "bun:test";
+import { dateFormat } from "@utils/dateFormat.js";
+
+// 日付フォーマット
+/**
+ * 引数
+ * - 対象時刻
+ * - 比較時刻 (デフォルトは現在時刻)
+ * 条件
+ * - 対象時刻との差分が1分未満の場合は「たった今」
+ * - 対象時刻との差分が1時間未満の場合は「○分前」
+ * - 対象時刻との差分が1日未満の場合は「○時間前」
+ * - 対象時刻との差分が1年未満の場合は「⚪︎月⚪︎日」
+ * - 対象時刻との差分が1年以上の場合は「⚪︎年⚪︎月⚪︎日」
+ * - 対象時刻が無効な場合は「不明」
+ */
+
+describe("dateFormat", () => {
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    const expected = "たった今";
+
+    const result = dateFormat(targetDate, targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    const expected = "たった今";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setSeconds(targetDate.getSeconds() - 59);
+    const expected = "たった今";
+
+    const result = dateFormat(targetDate, targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setMinutes(targetDate.getMinutes() - 1);
+    const expected = "1分前";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setMinutes(targetDate.getMinutes() - 2);
+    const expected = "2分前";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setHours(targetDate.getHours() - 1);
+    const expected = "1時間前";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setHours(targetDate.getHours() - 2);
+    const expected = "2時間前";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() - 1);
+    const expected = `${targetDate.getMonth() + 1}月${targetDate.getDate()}日`;
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setMonth(targetDate.getMonth() - 1);
+    const expected = `${targetDate.getMonth() + 1}月${targetDate.getDate()}日`;
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date();
+    targetDate.setMonth(targetDate.getMonth() - 1);
+    const expected = `${targetDate.getFullYear()}年${targetDate.getMonth() + 1}月${targetDate.getDate()}日`;
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("dateFormat", async () => {
+    const targetDate = new Date("invalid");
+    const expected = "不明";
+
+    const result = dateFormat(targetDate);
+    expect(result).toStrictEqual(expected);
+  });
+});
---
+++ test/extractTagsFromPost.test.js
@@ -0,0 +1,243 @@
+import { test, expect, describe } from "bun:test";
+
+import { extractTagsFromPost } from "@/utils/extractTagsFromPost.js";
+
+// タグ抽出
+/**
+ * 抽出条件
+ * - #または＃で始まる文字列
+ * - 連続する#または＃は1つにまとめる
+ * - 直前にスペースまたは改行があるか、文字列の先頭である
+ * - URLの中に含まれる#または＃はタグとして抽出しない
+ * - 複数のタグがある場合は、それぞれを抽出する
+ * - タグは重複しない
+ * - タグはアルファベット、数字のみ使用可能
+ * - タグは最大64文字まで
+ * - タグは大文字小文字を区別しない
+ * - タグは半角スペース、記号、改行で終了する
+ */
+
+describe("extractTagsFromPost", () => {
+  test("extractTagsFromPost", async () => {
+    const postContent = "Hello, #world!";
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = "Hello, #world! #world!";
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = "Hello, #world! #world! #world!";
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = "Hello, #world! #world! #world! #world!";
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = "Hello, world!";
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+    world!`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+    #world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+    # world!`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+  ＃world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+  ＃#world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello,
+	#＃world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world! https://example.com`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world! https://example.com/#world`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world! https://example.com/ #world`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world! https://example.com#world!`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world! https://example.com #world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world!#world!`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `Hello, world!
+#world!`;
+    const expected = ["world"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3`;
+    const expected = ["tag1", "tag2", "tag3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3 #tag1`;
+    const expected = ["tag1", "tag2", "tag3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3 #tag1 #tag2`;
+    const expected = ["tag1", "tag2", "tag3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3 #tag1 #tag2 #tag3`;
+    const expected = ["tag1", "tag2", "tag3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3 #タグ1 #タグ2 #タグ3`;
+    const expected = ["tag1", "tag2", "tag3", "タグ1", "タグ2", "タグ3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1 #tag2 #tag3 #タグ1 #タグ2 #タグ3 #tag1 #tag2 #tag3 #タグ1 #タグ2 #タグ3`;
+    const expected = ["tag1", "tag2", "tag3", "タグ1", "タグ2", "タグ3"];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1#tag2`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = ` #tag1#tag2`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+
+  test("extractTagsFromPost", async () => {
+    const postContent = `#tag1＃tag2`;
+    const expected = [];
+
+    const result = extractTagsFromPost(postContent);
+    expect(result).toStrictEqual(expected);
+  });
+});
