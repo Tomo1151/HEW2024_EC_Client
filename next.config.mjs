6e2707098ac85d295d2209ed45fccc1313634bf7
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-11-26 23:14:19 +0900

    add prod host to remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "api.syntck.miseba.com",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
