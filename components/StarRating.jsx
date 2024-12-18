005c9e4bdbf3f6870cdec59f91cafa25205803d6
honoka <shimajirou-honoken300930@au.com>
2024-12-18 15:03:30 +0900

    add rating object

--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -1,5 +1,13 @@
-const StarRating = () => {
-  return <div>StarRating</div>;
+import StarRateRoundedIcon from "@mui/icons-material/StarRateRounded";
+const StarRating = ({ rating }) => {
+  return (
+    <div>
+      <StarRateRoundedIcon className="text-yellow-500" />
+      <span className="text-xl inline-block h-full pl-2 align-top">
+        {rating}
+      </span>
+    </div>
+  );
 };
 
 export default StarRating;
