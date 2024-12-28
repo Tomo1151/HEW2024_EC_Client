912bd6425471dd317576909345d99b642c6581e3
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-12-28 19:33:50 +0900

    improvement: desktop header responsibility

--- components/MobileHeader.jsx
+++ components/MobileHeader.jsx
@@ -12,8 +12,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 
 const MobileHeader = ({ listItems, activeUser, isHeaderTransparent }) => {
   const [open, setOpen] = useState(false);
