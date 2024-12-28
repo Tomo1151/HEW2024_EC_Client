84ebc12cd6c413ee995e9dcad1b30b3ef8ecb820
Tomo <94550718+Tomo1151@users.noreply.github.com>
2024-12-28 23:45:57 +0900

    improvement: purchase-history page

--- app/purchase-history/page.jsx
+++ app/purchase-history/page.jsx
@@ -1,152 +1,171 @@
-import ArrowBackButton from "@/components/ArrowBackButton";
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
+
+import InfiniteScroll from "react-infinite-scroller";
 import PostCard from "@/components/PostCard";
 import MainColumnHeader from "@/components/MainColumnHeader";
+import CircularLoading from "@/components/loading/CircularLoading";
 
-export default function PurchaseHistory(props) {
-  const tentative = [
-    {
-      purchase_price: 3000,
-      created_at: "2024-12-04T05:56:28.283Z",
-      user: {
-        username: "test_user_0",
-        nickname: "テストユーザー0",
-        icon_link: "https://placeholder.com/150",
-      },
-      product: {
-        name: "商品_0",
-        price: 3100,
-        post: {
-          content: "商品_0についての説明",
-          images: [
-            {
-              image_link: "https://placeholder.com/150",
-            },
-          ],
-          author: {
-            username: "test_user_1",
-            nickname: "テストユーザー1",
-            icon_link: "https://placeholder.com/150",
-          },
-        },
-      },
-    },
-    {
-      purchase_price: 3200,
-      created_at: "2024-12-04T05:56:28.283Z",
-      user: {
-        username: "test_user_1",
-        nickname: "テストユーザー1",
-        icon_link: "https://placeholder.com/150",
-      },
-      product: {
-        name: "商品1名 ABCDEFG",
-        price: 3160,
-        post: {
-          content: "商品_1についての説明",
-          images: [
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-          ],
-          author: {
-            username: "test_user_0",
-            nickname: "テストユーザー0",
-            icon_link: "https://placeholder.com/150",
-          },
-        },
-      },
-    },
-    {
-      purchase_price: 31200,
-      created_at: "2024-12-04T05:56:28.283Z",
-      user: {
-        username: "test_user_2",
-        nickname: "テストユーザー2",
-        icon_link: "https://placeholder.com/150",
-      },
-      product: {
-        name: "商品名2 ABCDEFGHIJ",
-        price: 3160,
-        post: {
-          content: "商品_2についての説明",
-          images: [
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-          ],
-          author: {
-            username: "test_user_1",
-            nickname: "テストユーザー1",
-            icon_link: "https://placeholder.com/150",
-          },
-        },
-      },
-    },
-    {
-      purchase_price: 312100,
-      created_at: "2024-12-04T05:56:28.283Z",
-      user: {
-        username: "test_user_3",
-        nickname: "テストユーザー3",
-        icon_link: "https://placeholder.com/150",
-      },
-      product: {
-        name: "商品名3 ABCDEFGHIJ",
-        price: 3160,
-        post: {
-          content: "商品_3についての説明",
-          images: [
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-            {
-              image_link: "https://placeholder.com/150",
-            },
-          ],
-          author: {
-            username: "test_user_1",
-            nickname: "テストユーザー1",
-            icon_link: "https://placeholder.com/150",
-          },
-        },
-      },
-    },
-  ];
-  // tentative.map((user) => console.log(user));
+import { useUserContext } from "@/context/UserContext";
+
+import { fetchHeaders } from "@/config/fetchConfig";
+
+export default function PurchaseHistoryPage() {
+  const { refreshToken } = useUserContext();
+  const [isPostFetching, setIsPostFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [purchaseHistory, setPurchaseHistory] = useState([]);
+
+  const aggrigatePurchaseHistory = (purchaseHistory) => {
+    const result = purchaseHistory.reduce((acc, curr) => {
+      const date = new Date(curr.created_at).toLocaleDateString("ja-JP");
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(curr);
+      return acc;
+    }, {});
+
+    return result;
+  };
+
+  const fetchPurchaseHistory = async () => {
+    if (isPostFetching || !hasMore) return;
+    setIsPostFetching(true);
+    refreshToken().then(async () => {
+      const query = {
+        before:
+          purchaseHistory && purchaseHistory.length > 0
+            ? purchaseHistory[purchaseHistory.length - 1].id
+            : "",
+      };
+
+      const params = new URLSearchParams(query);
+
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_FETCH_BASE_URL + "/purchase?" + params,
+        {
+          method: "GET",
+          headers: fetchHeaders,
+          credentials: "include",
+        }
+      );
+
+      const resJson = await response.json();
+      // console.log(resJson);
+
+      if (resJson.success) {
+        // console.log(aggrigatePurchaseHistory(resJson.data));
+        setPurchaseHistory(purchaseHistory.concat(resJson.data));
+        setHasMore(resJson.data.length === 10);
+      }
+    });
+    setIsPostFetching(false);
+  };
+
+  useEffect(() => {
+    fetchPurchaseHistory();
+  }, []);
 
   return (
     <section className="bg-white">
       <MainColumnHeader className="font-bold tracking-wider">
         <h3>購入履歴</h3>
       </MainColumnHeader>
-      {tentative.map((buyHistory, index) => (
+      <InfiniteScroll
+        loadMore={fetchPurchaseHistory}
+        hasMore={hasMore}
+        loader={<CircularLoading key={0} />}
+      >
+        {Object.keys(aggrigatePurchaseHistory(purchaseHistory)).map(
+          (key, i) => {
+            return (
+              <div key={i}>
+                <h3 className="text-[1.25em] px-3 pt-4">{key}</h3>
+                {aggrigatePurchaseHistory(purchaseHistory)[key].map(
+                  (purchase) => (
+                    <PostCard
+                      key={purchase.id}
+                      // created_at={purchase.created_at} //購入日付
+                      image_link={
+                        purchase.product.thumbnail_link
+                          ? `${process.env.NEXT_PUBLIC_FETCH_BASE_URL}/media/images/${purchase.product.thumbnail_link}`
+                          : "https://placeholder.com/150"
+                      } //画像イメージ
+                      post_link={`/posts/${purchase.product.post.id}`}
+                      sellerName={purchase.product.post.author.username}
+                      sellerIcon={
+                        purchase.product.post.author.icon_link
+                          ? `${process.env.NEXT_PUBLIC_FETCH_BASE_URL}/media/icons/${purchase.product.post.author.icon_link}`
+                          : "https://placeholder.com/150"
+                      } //売り手のアイコン
+                      productName={purchase.product.name} //売り物の名前
+                      content={purchase.product.post.content} //売り物の詳細
+                    >
+                      <h2 className="text-[1.15em] ml-1">
+                        {purchase.product.name}
+                      </h2>
+                      <p className="ml-1 mb-2 text-red-500 font-bold">
+                        {purchase.product.price.toLocaleString("ja-JP", {
+                          style: "currency",
+                          currency: "JPY",
+                        })}
+                      </p>
+                      <p className="text-[1em] ml-1">
+                        {purchase.product.post.content}
+                      </p>
+                    </PostCard>
+                  )
+                )}
+              </div>
+            );
+          }
+        )}
+        {/* {purchaseHistory?.map((purchase) => (
+          <PostCard
+            key={purchase.id}
+            created_at={purchase.created_at} //購入日付
+            image_link={
+              purchase.product.thumbnail_link
+                ? `${process.env.NEXT_PUBLIC_FETCH_BASE_URL}/media/images/${purchase.product.thumbnail_link}`
+                : "https://placeholder.com/150"
+            } //画像イメージ
+            sellerName={purchase.product.post.author.username}
+            sellerIcon={
+              purchase.product.post.author.icon_link
+                ? `${process.env.NEXT_PUBLIC_FETCH_BASE_URL}/media/icons/${purchase.product.post.author.icon_link}`
+                : "https://placeholder.com/150"
+            } //売り手のアイコン
+            productName={purchase.product.name} //売り物の名前
+            content={purchase.product.post.content} //売り物の詳細
+          >
+            <h2 className="text-[1.15em] ml-1">{purchase.product.name}</h2>
+            <p className="ml-1 mb-2 text-red-500 font-bold">
+              {purchase.product.price.toLocaleString("ja-JP", {
+                style: "currency",
+                currency: "JPY",
+              })}
+            </p>
+            <p className="text-[1em] ml-1">{purchase.product.post.content}</p>
+          </PostCard>
+        ))} */}
+      </InfiniteScroll>
+
+      {/* {tentative.map((purchase, index) => (
         <PostCard
           key={index}
-          created_at={buyHistory.created_at} //購入日付
-          image_link={buyHistory.product.post.images[0].image_link} //画像イメージ
-          sellerName={buyHistory.product.post.author.username} //売り手の名前
-          sellerIcon={buyHistory.product.post.author.icon_link} //売り手のアイコン
-          productName={buyHistory.product.name} //売り物の名前
-          content={buyHistory.product.post.content} //売り物の詳細
+          created_at={purchase.created_at} //購入日付
+          image_link={purchase.product.post.images[0].image_link} //画像イメージ
+          sellerName={purchase.product.post.author.username} //売り手の名前
+          sellerIcon={purchase.product.post.author.icon_link} //売り手のアイコン
+          productName={purchase.product.name} //売り物の名前
+          content={purchase.product.post.content} //売り物の詳細
         ></PostCard>
-      ))}
+      ))} */}
     </section>
   );
 }
