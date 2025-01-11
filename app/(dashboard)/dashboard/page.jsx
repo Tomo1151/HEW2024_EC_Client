ac1c49431535262189dd1aaf0301ea1911a77de6
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-01-11 22:24:56 +0900

    init dashboard page

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { Container } from "@mui/material";
+
+import { useUserContext } from "@/context/UserContext";
+import { urlForImage } from "@/utils/utils";
 
 const page = () => {
-  return <div>page</div>;
+  const { activeUser } = useUserContext();
+  return (
+    <Container
+      sx={{
+        backgroundColor: "#f0f0f0",
+        maxWidth: "1000px",
+        px: 4,
+        py: 2,
+        mx: "auto",
+      }}
+    >
+      <h1>Dashboard</h1>
+      <Link href={`/users/${activeUser?.username}`}>
+        <Image
+          src={urlForImage(activeUser?.icon_link)}
+          width={150}
+          height={150}
+          alt="ユーザーアイコン"
+          priority
+        />
+      </Link>
+      <p>
+        This is a dashboard page. It is a protected route that is only
+        accessible to authenticated users.
+      </p>
+    </Container>
+  );
 };
 
 export default page;
