6a0f4f050c46b79b6897930c874e31ffae0a763b
kaka_256 <kumarai460@gmail.com>
2025-01-18 18:29:25 +0900

    add ogp api

---
+++ app/api/ogp/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams;
+  const res = await fetch(searchParams.get("url"));
+  const text = await res.text();
+  const ogpTag = text.match(
+    /<meta property="og:[\u0000-\u0019\u0021-\uFFFF]+" content="[\u0000-\u0019\u0021-\uFFFF]+"\/>/gu
+  );
+
+  const ogpDict = {};
+  ogpTag.forEach((tag) => {
+    const index = tag.match(
+      /(?<=property="og:)[\u0000-\u0019\u0021-\uFFFF]+(?=")/
+    )[0];
+    const data = tag.match(
+      /(?<=content=")[\u0000-\u0019\u0021-\uFFFF]+(?=")/
+    )[0];
+    ogpDict[index] = data;
+  });
+
+  return NextResponse.json(ogpDict);
+}
