27ab9ecaa18f8daa860d3a2b9b6296b7ab471910
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-01-25 23:28:25 +0900

    Refactor Media component to display image with main column header

--- app/(main)/media/[image]/page.jsx
+++ app/(main)/media/[image]/page.jsx
@@ -1,5 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { urlForImage } from "@/utils/utils";
+import MainColumnHeader from "@/components/MainColumnHeader";
+
 const Media = () => {
-  return <div>Mediaa</div>;
+  const { image } = useParams();
+
+  return (
+    <>
+      <MainColumnHeader>
+        <h3 className="font-bold tracking-wider">画像</h3>
+      </MainColumnHeader>
+      <Image
+        src={urlForImage(image, "images")}
+        width={800}
+        height={800}
+        className="block w-full object-contain object-center select-none pointer-events-none"
+        alt="投稿画像"
+      />
+    </>
+  );
 };
 
 export default Media;
