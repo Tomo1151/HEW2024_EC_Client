3bf0b9aa2073832b7cfbd55aa1a676ac593c2def
kaka_256 <kumarai460@gmail.com>
2025-02-09 19:13:47 +0900

    upgrade: add text

--- app/(main)/liveHelp/page.jsx
+++ app/(main)/liveHelp/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-
+import Image from "next/image";
 import { Box, ThemeProvider } from "@mui/material";
 
 import MainColumnHeader from "@/components/MainColumnHeader";
@@ -12,7 +12,39 @@ const liveHelp = () => {
       <MainColumnHeader>
         <h3 className="font-bold tracking-wider">ヘルプ</h3>
       </MainColumnHeader>
-      <Box className="m-[2rem]"></Box>
+      <Box className="m-[2rem]">
+        <h1 className="text-3xl">ライブ出品とは？</h1>
+        <br />
+        <p>
+          Youtubeのサービスを利用して実際に作っている様子を配信しながら出品をすることができるサービスです。商品サムネイルの変わりに設定したYoutubeの埋め込みがされます。
+        </p>
+        <br />
+        <h2 className="text-2xl">ライブ配信をする</h2>
+        <br />
+        <ol className="list-decimal list-inside">
+          <li>
+            Youtubeの場合は共有ボタンを押して作成されるURLをコピーします
+            <Image
+              src="/live_help/share.png"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt="ユーザーアイコン"
+            />
+          </li>
+          <br />
+          <li>
+            ライブ出品モードに変更し「ライブURL」の項目に入力して、出品します
+            <Image
+              src="/live_help/new_post.png"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt="ユーザーアイコン"
+            />
+          </li>
+        </ol>
+      </Box>
     </ThemeProvider>
   );
 };
