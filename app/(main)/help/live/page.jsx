11486725d2dc10746de2b5cf490bbbafe7e83f3d
kaka_256 <kumarai460@gmail.com>
2025-02-12 15:26:16 +0900

    fix: css, add twich text

--- app/(main)/help/live/page.jsx
+++ app/(main)/help/live/page.jsx
@@ -18,32 +18,32 @@ const liveHelp = () => {
         <p>
           Youtubeのサービスを利用して実際に作っている様子を配信しながら出品をすることができるサービスです。商品サムネイルの代わりに、設定したYoutubeの埋め込みが表示されます。
         </p>
-        <br />
-        <h2 className="text-2xl">ライブ配信をする</h2>
-        <br />
-        <ol className="list-decimal list-inside">
-          <li>
-            Youtubeの場合は共有ボタンを押して作成されるURLをコピーします
-            <Image
-              src="/live_help/share.png"
-              width={500}
-              height={500}
-              className="object-cover w-full h-full"
-              alt="ユーザーアイコン"
-            />
-          </li>
-          <br />
-          <li>
-            ライブ出品モードに変更し「ライブURL」の項目に入力して、出品します
-            <Image
-              src="/live_help/new_post.png"
-              width={500}
-              height={500}
-              className="object-cover w-full h-full"
-              alt="ユーザーアイコン"
-            />
-          </li>
-        </ol>
+        <Box className="my-[2rem] py-[2rem] border-t border-gray-300">
+          <h2 className="text-2xl">ライブ配信をする</h2>
+          <ol className="list-decimal list-outside ml-[1.5rem]">
+            <li className="mt-[2rem]">
+              Youtubeの場合は共有ボタンを押して作成されるURLをコピーします
+              Twitchの場合はユーザーページのURLをコピーします
+              <Image
+                src="/live_help/share.png"
+                width={500}
+                height={500}
+                className="object-cover w-full h-full"
+                alt="ユーザーアイコン"
+              />
+            </li>
+            <li className="mt-[2rem]">
+              ライブ出品モードに変更し「ライブURL」の項目に入力して、出品します
+              <Image
+                src="/live_help/new_post.png"
+                width={500}
+                height={500}
+                className="object-cover w-full h-full"
+                alt="ユーザーアイコン"
+              />
+            </li>
+          </ol>
+        </Box>
       </Box>
     </ThemeProvider>
   );
